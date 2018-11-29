99b76eef149e202b4c1b77aa2b5e5b42e178c678
bzouleka <bzouleka@gmail.com>
2018-11-29 13:29:23 +0100

    add delete button calendar-event-page

--- src/app/home/couche-db.service.ts
+++ src/app/home/couche-db.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { Observable} from 'rxjs/internal/Observable';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/internal/Observable';
+
+const username = 'papetagendazv_zouleka';
+const password = 'campus01';
+const httpOptions = {
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + btoa(`${username}:${password}`)
+    })
+};
 
 @Injectable()
 export class CoucheDbService {
 
     urlEvents = 'http://papetagendazv.alwaysdata.net/data/papetagendazv_agenda/_all_docs?include_docs=true';
     baseUrl = 'http://papetagendazv.alwaysdata.net/data/papetagendazv_agenda/';
-    constructor(private http: HttpClient) { }
+
+    constructor(private http: HttpClient) {
+    }
 
     getEvents(): Observable<any> {
         return this.http.get<any>(this.urlEvents);
     }
 
-    deleteEvent(event: any): Observable<any> {
-        return this.http.delete<any>(this.baseUrl + event._id);
+    deleteEvent(event: any): Observable<Response> {
+        const delUrl = this.baseUrl + event._id + '?rev=' + event._rev;
+        return this.http.delete<Response>(delUrl, httpOptions);
     }
 }
